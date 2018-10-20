e1edc7dd5457fc739ce6f4fe4fea7ac52be6b711
Huynh Minh Huan <minhhuanhuynh289@gmail.com>
2018-10-20 19:00:02 +0700

    Q[id] < Q[(id - 1)/2]

--- Lab01/py/MyQueue.py
+++ Lab01/py/MyQueue.py
@@ -1,46 +1,42 @@
 class PriorityQueue:
     def __init__(self):
         self.Q = []
+        self.size = 0
     def put(self, item):
         self.Q.append(item)
         id = len(self.Q) - 1
-        while (id > 0) and (self.Q[id] < self.Q[int(id/2)]) :
+        while (id > 0) and (self.Q[int(id)] < self.Q[int((id - 1)/2)]):
             temp = self.Q[id]
-            self.Q[id] = self.Q[int(id/2)]
-            self.Q[int(id/2)] = temp
-            id = int(id/2)
-        # for i in range(len(self.Q)):
-        #     print(self.Q[i],end=" ")
-        # print()
+            self.Q[id] = self.Q[int((id - 1)/2)]
+            self.Q[int((id - 1)/2)] = temp
+            id = int((id - 1)/2)
+        self.size = len(self.Q)
     def __getitem__(self, id):
         return self.Q[id]
     def get(self):
         val = self.Q[0]
-        self.Q.pop()
-        if (len(self.Q) != 0):
+        if (len(self.Q) > 1):
+            self.Q[0] = self.Q[self.size - 1]
+            self.Q.pop()
             n, id = len(self.Q) - 1, 0
-            self.Q[0] = self.Q[n]
-            while (id * 2 + 1 <= n and self.Q[id] > self.Q[id * 2 + 1]) or (id * 2 + 2 <= n and self.Q[id] > self.Q[id *2 + 2]):
-                temp = self.Q[id]
-                if (self.Q[id * 2 + 1] < self.Q[id * 2 + 2]):
-                    self.Q[id] = self.Q[id * 2 + 1]
-                    id = id * 2 + 1 
-                    self.Q[id] = temp
+            while True:
+                left, right, temp = id * 2 + 1, id * 2 + 2, self.Q[id]
+                if (left <= n and temp < self.Q[left]) and (right <= n and self.Q[right] > temp):
+                    break
+                if (left <= n):
+                    if (right <= n) and (self.Q[right] < self.Q[left]):
+                        self.Q[id] = self.Q[right]
+                        self.Q[right] = temp
+                        id = right
+                    else:
+                        self.Q[int(id)] = self.Q[int(left)]
+                        self.Q[left] = temp
+                        id = left
                 else:
-                    self.Q[id] = self.Q[id * 2 + 2]
-                    id = id * 2 + 2
-                    self.Q[id] = temp
-            
+                    break
+        else:
+            self.Q.pop()
+        self.size = len(self.Q)
         return val
-    def _print(self):
-        for i in range(len(self.Q)):
-            print(self.Q[i], end=" ")
-        print()
     def empty(self):
         return (len(self.Q) == 0)
-    def size(self):
-        return len(self.Q)
-
-
-
-
